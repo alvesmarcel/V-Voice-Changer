48686bd5bc09833c0f0c93a35908e427a13acdf1
Marcel Alves <marcel.alves89@gmail.com>
2019-03-31 11:04:34 -0300

    Implement RecordViewController
    Implement recordButtonTapped action, and audioRecorder configuration and behavior.

--- V Voice Changer/Controller/RecordViewController.swift
+++ V Voice Changer/Controller/RecordViewController.swift
@@ -1,12 +1,44 @@
 import UIKit
+import AVFoundation
 
 class RecordViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    private var audioRecorder: AVAudioRecorder!
+    private var audioSession = AVAudioSession.sharedInstance()
 }
 
+// MARK: - Actions
+extension RecordViewController {
+    @IBAction func recordButtonTapped(_ sender: UIButton) {
+        if audioRecorder == nil {
+            configureAudioRecorder()
+        }
+        if self.audioRecorder.isRecording {
+            self.audioRecorder.stop()
+        } else {
+            self.audioRecorder.record()
+        }
+    }
+}
+
+// MARK: - AVAudioRecorderDelegate
+extension RecordViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            // Should go to the next view controller
+        } else {
+            print("There was a problem recording the audio")
+        }
+    }
+}
+
+// MARK: - Helpers
+extension RecordViewController {
+    func configureAudioRecorder() {
+        // This stops any other audio that is currently playing; should be called only when the user taps the record button
+        try! self.audioSession.setActive(true)
+        guard let audioURL = PersistenceManager.shared.getCurrentFileURL() else { return }
+        let recordSettings = [AVFormatIDKey : kAudioFormatMPEG4AAC]
+        try! self.audioRecorder = AVAudioRecorder(url: audioURL, settings: recordSettings)
+        self.audioRecorder.delegate = self
+    }
+}
