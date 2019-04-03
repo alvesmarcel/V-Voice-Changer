8f98f8426d4da5177d31179c00338d719b7c6023
Marcel Alves <marcel.alves89@gmail.com>
2019-04-03 17:06:42 -0300

    Finish implementation of PlayerViewController
    Organize the class PlayerViewController, while also implements the main function (playButtonTapped).

--- V Voice Changer/Controller/PlayerViewController.swift
+++ V Voice Changer/Controller/PlayerViewController.swift
@@ -3,22 +3,43 @@ import UIKit
 class PlayerViewController: UIViewController {
     var recordedAudio: AudioFile?
     private var audioProcessor: AudioFXProcessor?
-    
+    private enum ButtonIdentifier: String {
+        case TurtleButton, RabbitButton, AlienButton, DarthButton, ShaoButton, JigsawButton
+    }
+}
+
+// MARK: - Lifecycle
+extension PlayerViewController {
     override func viewDidLoad() {
-        //let audioFile = try! PersistenceManager.shared.createNewAudioFile()
-       if let audioFile = recordedAudio {
+        if let audioFile = recordedAudio {
             do {
                 try self.audioProcessor = AudioFXProcessor(audioFile: audioFile)
             } catch {
                 print("Error instantiating the AudioFXProcessor\n\(error.localizedDescription)")
             }
-       }
+        }
     }
-    
-    @IBAction func playButtonTapped(_ sender: Any) {
+}
+
+// MARK: - Actions
+extension PlayerViewController {
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+        guard let identifier = sender.restorationIdentifier, let enumId = ButtonIdentifier(rawValue: identifier) else { return }
         var effect: AudioFXProcessor.Effects = .none
-        
-        audioProcessor?.play(withEffect: .chipmunk)
+        switch enumId {
+        case .TurtleButton:
+            effect = .slow
+        case .RabbitButton:
+            effect = .fast
+        case .AlienButton:
+            effect = .chipmunk
+        case .DarthButton:
+            effect = .darthvader
+        case .ShaoButton:
+            effect = .shaokahn
+        case .JigsawButton:
+            effect = .jigsaw
+        }
+        audioProcessor?.play(withEffect: effect)
     }
-    
 }
