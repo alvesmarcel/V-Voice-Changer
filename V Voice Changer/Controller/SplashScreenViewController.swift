173b6ef149b441978f518875cc92915c09a9006e
Marcel Alves <marcel.alves89@gmail.com>
2019-04-04 10:44:48 -0300

    Improve SplashScreenViewController
    Add animationView as an outlet, and remove unnecessary code.

--- V Voice Changer/Controller/SplashScreenViewController.swift
+++ V Voice Changer/Controller/SplashScreenViewController.swift
@@ -3,21 +3,18 @@ import Lottie
 
 class SplashScreenViewController: UIViewController {
     
-    @IBOutlet weak var animatedView: UIView!
+    @IBOutlet weak var animationView: AnimationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animationView.animation = Animation.named("audio_wave_animation")
+        animationView.animationSpeed = 0.6
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let animationView = AnimationView(name: "audio_wave_animation")
-        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 300)
-        animationView.center = self.view.center
-        animationView.animationSpeed = 0.6
-        //animationView.contentMode = .scaleAspectFill
+        
         //animationView.loopMode = LottieLoopMode.loop
-        self.view.addSubview(animationView)
         animationView.play { [weak self] (success) in
             DispatchQueue.main.async {
                 guard let playerVC = self?.storyboard?.instantiateViewController(withIdentifier: "NavigationController") as? UINavigationController else {
