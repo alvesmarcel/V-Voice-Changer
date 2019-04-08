0ff04e75b8716d93ae741456dbd3898ee7e42a67
Marcel Alves <marcel.alves89@gmail.com>
2019-04-08 12:57:57 -0300

    Update PersistenceManager to include urlForFile method

--- V Voice Changer/Model/PersistenceManager.swift
+++ V Voice Changer/Model/PersistenceManager.swift
@@ -3,18 +3,22 @@ import Foundation
 class PersistenceManager {
     
     private(set) var currentAudioFile: AudioFile?
-    private let fileNumberKey = "FileNumberKey"
     private let fileExtension = ".m4a"
     
     // Singleton pattern
     private init() {}
     static let shared = PersistenceManager()
     
-    func createNewAudioFile() throws -> AudioFile {
-        let fileName = "V\(fileExtension)"
+    func urlForFile(named fileName: String) throws -> URL {
+        let fileName = "\(fileName)\(fileExtension)"
         let fileManager = FileManager.default
         let documentDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false)
-        let fileURL = documentDirectory.appendingPathComponent(fileName)
+        return documentDirectory.appendingPathComponent(fileName)
+    }
+    
+    func createNewAudioFile() throws -> AudioFile {
+        let fileName = "V"
+        let fileURL = try urlForFile(named: fileName)
         self.currentAudioFile = AudioFile(name: fileName, url: fileURL)
         return self.currentAudioFile!
     }
