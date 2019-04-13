ac8055bdaf6dd5c0bd1d7fe7d6b4fb0c159f5db0
Marcel Alves <marcel.alves89@gmail.com>
2019-04-13 11:22:01 -0300

    Adjust JigsawEffect
    Change the values of Jigsaw effect to improve the result.

--- V Voice Changer/Controller/AudioProcessor/Effects/JigsawEffect.swift
+++ V Voice Changer/Controller/AudioProcessor/Effects/JigsawEffect.swift
@@ -5,11 +5,11 @@ class JigsawEffect: Effect {
     private(set) var rate = 1.0
     private(set) lazy var audioUnits: [AVAudioUnit] = {
         let distortionAU = AVAudioUnitDistortion()
-        distortionAU.loadFactoryPreset(.drumsBitBrush)
-        distortionAU.wetDryMix = 13
+        distortionAU.loadFactoryPreset(.speechWaves)
+        distortionAU.wetDryMix = 20
         
         let timePitchAU = AVAudioUnitTimePitch()
-        timePitchAU.pitch = -343
+        timePitchAU.pitch = -300
         
         return [distortionAU, timePitchAU]
     }()
